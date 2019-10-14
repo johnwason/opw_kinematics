124da5ce47346388f49cd1b8674066dd8e708944
John Wason <wason@wasontech.com>
2019-10-14 16:45:12 -0400

    Add Windows CI tests

--- travis_ci_windows_install.bat
+++ travis_ci_windows_install.bat
@@ -2,7 +2,7 @@
 
 call c:\opt\ros\melodic\x64\setup.bat
 
-rosdep install --from-paths src --ignore-src -r -y
+rosdep install --from-paths src --ignore-src -r -y -q > nul
 
-c:\opt\vcpkg\bootstrap-vcpkg.bat
-c:\opt\vcpkg\vcpkg install jsoncpp
+c:\opt\vcpkg\bootstrap-vcpkg.bat > nul
+c:\opt\vcpkg\vcpkg install jsoncpp:x64-windows > nul
