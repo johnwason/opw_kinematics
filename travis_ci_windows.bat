53ab0a76d5577faaa7c52598926347f52a247a91
John Wason <wason@wasontech.com>
2019-10-15 14:12:53 -0400

    Update travis.yml for OSX

--- travis_ci_windows.bat
+++ travis_ci_windows.bat
@@ -2,4 +2,4 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 call c:\opt\ros\melodic\x64\setup.bat
-catkin_make_isolated -DBUILD_TESTS=ON
+catkin_make_isolated -DENABLE_TESTS=ON
